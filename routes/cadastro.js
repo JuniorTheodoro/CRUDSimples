10c65814285cf3e56fe6ecc5303542eff07af253
Júnior Theodoro <juninho_lpisrock@hotmail.com>
2019-04-22 19:50:13 -0300

    Realizando cadastros no bd

--- routes/cadastro.js
+++ routes/cadastro.js
@@ -3,10 +3,15 @@ var router = express.Router();
 var novoCliente = require('../models/ModelCrud')
 
 router.post('/salvar', async function (req, res) {
-  let { NovoCliente } = req.body;
-  var cliente = new novoCliente();
-  var result = await cliente.Save('cliente', NovoCliente);
-  console.log(result);
+  let { Nome, DtCadastro, Telefone, CpfCnpj, Celular, Ie, Logradouro, Bairro, Complemento, Cep, Cidade, Uf, DtNascimento, Bloqueado, Obs, Email } = req.body;
+  try {
+    var cliente = new novoCliente();
+    var result = await cliente.Save('cliente', { Nome, DtCadastro, Telefone, CpfCnpj, Celular, Ie, Logradouro, Bairro, Complemento, Cep, Cidade, Uf, DtNascimento, Bloqueado, Obs, Email });
+    console.log(result);
+    res.json(result)
+  } catch (error) {
+    res.json(error)
+  }
 });
 
 module.exports = router;
